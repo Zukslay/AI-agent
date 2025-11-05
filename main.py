f830b6e015a27909c3555b7b13bfb99278a3be19
Zukslay <uksturmanboy@gmail.com>
2025-11-05 17:34:26 -0400

    sixth commit: added system-prompt and first function calling

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types
+from google.genai import types  
+from prompts import system_prompt
+from call_function import available_functions
 
 def main():
+    load_dotenv()
     
     verbose = '--verbose' in sys.argv or '-v' in sys.argv
     
@@ -13,7 +16,6 @@ def main():
         if not arg.startswith("--") and not arg.startswith('-'):
             user_prompt = arg
             break
-    
     if not user_prompt:
         print("Error: cero arguments provided")
         exit(1)
@@ -24,17 +26,25 @@ def main():
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
-    load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-    print(response.text)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions],
+                                           system_instruction=system_prompt))
     if verbose:
-        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if not response.function_calls:
+        return response.text
+        
+    for function in response.function_calls:
+        print(f"Calling function: {function.name}({function.args})")
+    
 
 
     
